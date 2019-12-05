1c22f10a32f99fc421a3df6b217bfad8ef2f76a7
msvihra <msvihra@qualitytunit.com>
2019-12-05 14:58:24 +0100

    changed docker install location

--- docker-1_host/templates/apache-fpm/install.sh
+++ docker-1_host/templates/apache-fpm/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /var/www/liveagent/scripts
+cd /var/www/liveagent/install
 php docker_install.php '{"name":"ADMIN_NAME","email":"ADMIN_EMAIL","password":"ADMIN_PASSWORD","licenseCode":"LICENSE_CODE","es_host":"elasticsearch","es_port":"9200","redis_host":"redis","redis_port":"6379","perf_tracking":"R","domain":"SERVER_NAME","resque_redis_backend":"redis:6379"}'
